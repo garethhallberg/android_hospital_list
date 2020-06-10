dd7b16416fe8aed4c4d5e350d4b72733eecb73a7
Gareth Hallberg <gareth@ghallberg.co.uk>
2020-06-10 09:25:02 +0100

    Hospital entity

---
+++ app/src/main/java/com/example/hospitallistviewer/Hospital.kt
@@ -0,0 +1,31 @@
+package com.example.hospitallistviewer
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "hospital_table")
+class Hospital(@PrimaryKey @ColumnInfo(name = "hospital")
+               val organisation_id: String,
+               val organisation_code: String,
+               val organisation_type: String,
+               val sub_type: String,
+               val sector: String,
+               val organisation_status: String,
+               val is_pims_managed: String,
+               val organisation_name: String,
+               val address1: String,
+               val address2: String?,
+               val address3: String?,
+               val city: String?,
+               val county: String?,
+               val postcode: String?,
+               val latitude: String?,
+               val longitude: String?,
+               val parent_ods_code: String?,
+               val parent_name: String?,
+               val phone: String?,
+               val email: String?,
+               val website: String?,
+               val fax: String?
+)
